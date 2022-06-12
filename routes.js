f31f86f517ab9ef15960bf8f45372d0267839714
kevin <kwagner1312@gmail.com>
2022-06-11 21:18:14 -0500

    initializes npm and adds nodemon package

--- routes.js
+++ routes.js
@@ -29,8 +29,6 @@ const requestHandler = (req, res) => {
         return res.end();
     }
     
-    // console.log('hello world in console!');
-    // console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
