2797953fc2193b30348308fc0bbd3ccf761c2c33
kevin <kwagner1312@gmail.com>
2022-06-18 16:48:32 -0500

    add express.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes);
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware!');
+    res.send('<h1 style="color: black; background: yellow;">hello Zoey!!!</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In the next middleware!');
+    res.send('<h1>hello from express</h1>');
+});
+
+app.listen(3000);
